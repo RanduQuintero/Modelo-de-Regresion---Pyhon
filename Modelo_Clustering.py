9327e5acd2b665efe8a4b2f44b3216e10d7b92fa
RanduQuintero <randuquintero@hotmail.com>
2026-02-05 18:50:58 -0700

    Archivo con mas datos (df)

--- Modelo_Clustering.py
+++ Modelo_Clustering.py
@@ -1,23 +1,22 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 import joblib
+from sklearn.metrics import silhouette_score
 
 #Dataframe improvisado
-df = pd.DataFrame({
-    "edad": [23, 25, 31, 45, 52, 48, 33, 29],
-    "ingreso": [12000, 15000, 22000, 48000, 52000, 50000, 26000, 21000],
-    "gasto_mensual": [2000, 2500, 3200, 6000, 6800, 6400, 3500, 3000]
-})
+df = pd.read_csv(r"C:\Users\Randu\Desktop\Modelos de machine learning\datos_clientes.csv")
+
 clusters = {
     0: "Clientes jóvenes - bajo gasto",
     1: "Clientes medio - gasto moderado",
     2: "Clientes premium - alto gasto"
 }
 #Uso de una pipe para que todos sigan el mismo proceso 
-# y lo que pasa internamnte
+# y lo que pasa internamente
 '''
 StandardScaler.fit(df)
 StandardScaler.transform(df)
@@ -27,7 +26,7 @@ pipeline = Pipeline([
     ("scaler", StandardScaler()),
     ("kmeans", KMeans(n_clusters=3))
 ])
-
+#Entrenamos el modelo
 pipeline.fit(df)
 #Almacenar el mmodelo entrenado
 joblib.dump(pipeline, "modelo_clustering.pkl")
@@ -37,8 +36,8 @@ modelo = joblib.load("modelo_clustering.pkl")
 #Cliente a asiganar 
 nuevo_cliente = pd.DataFrame([{
     "edad": 50,
-    "ingreso": 55000,
-    "gasto_mensual": 28000
+    "ingreso": 20000,
+    "gasto_mensual": 15000
 }])
 
 cluster = modelo.predict(nuevo_cliente)
